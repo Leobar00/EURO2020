dbe0ffa578a483e6c999e4aa086b9d05826abeda
Leonardo Barbieri <leonardo.barbieri@boraso.com>
2023-04-24 15:53:49 +0200

    Setting for use Vue on Symfony

---
+++ assets/vue/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Navbar from './components/Header'
+export default {
+  name: "App",
+  components: {
+    'Header': Navbar
+  },
+  props: {
+    title: String
+  },
+  data(){
+    return {
+      message:'bello'
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
